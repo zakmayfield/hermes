1d5b693a714205a0c4e10856bcdaa275395a169c
Zak Mayfield <zakmayfield@gmail.com>
2024-09-18 14:43:29 -0400

    rename test-route

--- src/app/test-route/layout.tsx
+++ src/app/test-route/layout.tsx
@@ -4,8 +4,8 @@ export default function TestLayout({ children }: { children: React.ReactNode }) 
   return (
     <div>
       <nav>
-        <Link href="/test">Test Home</Link>
-        <Link href="/test/containers">Containers</Link>
+        <Link href="/test-route">Test Home</Link>
+        <Link href="/test-route/containers">Containers</Link>
       </nav>
 
       <div>{children}</div>

f9d8ae76f620e690e2a565a916cf257bd07cfbb6
Zak Mayfield <zakmayfield@gmail.com>
2024-09-27 15:29:50 -0400

    optimize useIcons

--- src/tw-styled/hooks/useIcons.ts
+++ src/tw-styled/hooks/useIcons.ts
@@ -1,19 +1,21 @@
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { IconType } from "react-icons";
 import { PiSpinnerGap, PiShoppingCart, PiWarningCircle } from "react-icons/pi";
 
 type IconName = "spin" | "cart" | "error";
 
 export const useIcons = (names: IconName[]) => {
-  const iconMap: Record<IconName, IconType> = {
-    spin: PiSpinnerGap,
-    cart: PiShoppingCart,
-    error: PiWarningCircle
-  };
+  const iconMap = useCallback(() => {
+    return {
+      spin: PiSpinnerGap,
+      cart: PiShoppingCart,
+      error: PiWarningCircle
+    };
+  }, []);
 
   const icons = useMemo(() => {
     return names.reduce((acc, name) => {
-      return { ...acc, [name]: iconMap[name] };
+      return { ...acc, [name]: iconMap()[name] };
     }, {}) as Record<IconName, IconType>;
   }, [names, iconMap]);
 

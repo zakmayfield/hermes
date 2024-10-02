3abff14b9461fbca862be61a9cd4eff3291041f4
Zak Mayfield <zakmayfield@gmail.com>
2024-10-02 07:42:00 -0400

    optimize fielderror

--- src/tw-styled/components/wrappers/FieldError.tsx
+++ src/tw-styled/components/wrappers/FieldError.tsx
@@ -1,5 +1,6 @@
 import { useStyleResolver, useStyles } from "@/tw-styled";
 import { StyleProps } from "@/tw-styled/types";
+import { useMemo } from "react";
 
 export type FieldErrorProps = {
   message?: string;
@@ -19,13 +20,17 @@ export const FieldError = (props: FieldErrorProps) => {
   });
   const classes = useStyleResolver({ ...styles });
 
-  return (
-    <p
-      aria-describedby={described_by}
-      hidden={is_error_hidden}
-      className={classes.parentWrapper}
-    >
-      {message}
-    </p>
-  );
+  const FieldError = useMemo(() => {
+    return (
+      <p
+        aria-describedby={described_by}
+        hidden={is_error_hidden}
+        className={classes.parentWrapper}
+      >
+        {message}
+      </p>
+    );
+  }, [described_by, is_error_hidden, message, classes]);
+
+  return FieldError;
 };

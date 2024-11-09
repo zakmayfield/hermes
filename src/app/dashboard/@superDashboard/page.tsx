4b836ca46c24ed76ab886662cb45c403b7bbe99e
Zak Mayfield <zakmayfield@gmail.com>
2024-11-09 11:11:29 -0500

    todo

--- src/app/dashboard/@superDashboard/page.tsx
+++ src/app/dashboard/@superDashboard/page.tsx
@@ -1,13 +1,33 @@
 import { UserAnalytics } from "@/features/dashboard/super/user-analytics/templates";
-import { Layout } from "@/ui";
+import { Heading, Layout } from "@/ui";
 
 export default function Page() {
   return (
     <Layout
       options={{ title: <h2>Overview</h2> }}
-      style={{ parentWrapper: { spaceY: "lg" } }}
+      style={{ parentWrapper: { spaceY: "lg" }, bodyWrapper: { spaceY: "lg" } }}
     >
       <UserAnalytics />
+
+      <Heading
+        as="h3"
+        text="Order Analytics"
+      />
+
+      <Heading
+        as="h3"
+        text="Popular Product Analytics"
+      />
+
+      <Heading
+        as="h3"
+        text="Onboarding Analytics"
+      />
+
+      <Heading
+        as="h3"
+        text="Cart Analytics"
+      />
     </Layout>
   );
 }

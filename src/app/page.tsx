c052d35b75f183aad6ebdfe3ec590560ee276484
Zak Mayfield <zakmayfield@gmail.com>
2024-11-19 11:44:48 -0500

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { GetPokemons } from "@/features/examples/GetPokemons";
-
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
-
-      <GetPokemons />
     </div>
   );
 }

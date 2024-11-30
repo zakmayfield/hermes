94762dedaea6dd28388875053688af99e28c830f
Zak Mayfield <zakmayfield@gmail.com>
2024-11-30 16:49:07 -0500

    remove col from user table

---
+++ prisma/migrations/20241130182538_remove_company_name_col_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `company_name` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "company_name";

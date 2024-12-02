95166d6b7b0175247bab561264f0ba7337db2f05
Zak Mayfield <zakmayfield@gmail.com>
2024-12-02 17:41:45 -0500

    update qb sync record getter

--- src/data/database/queries/getQuickBooksCustomerSyncRecordByUserId.ts
+++ src/data/database/queries/getQuickBooksCustomerSyncRecordByUserId.ts
@@ -1,11 +1,21 @@
 "use server";
 
 import { db } from "@/lib/prisma";
+import { QuickbooksCustomerSync } from "@prisma/client";
 
 export const getQuickBooksCustomerSyncRecordByUserId = async ({
   user_id
 }: {
   user_id: string;
-}) => {
-  return await db.quickbooksCustomerSync.findUnique({ where: { user_id } });
+}): Promise<QuickbooksCustomerSync | null> => {
+  try {
+    const record = await db.quickbooksCustomerSync.findUnique({ where: { user_id } });
+    return record;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+
+    throw new Error("Unexpected Server Error");
+  }
 };
